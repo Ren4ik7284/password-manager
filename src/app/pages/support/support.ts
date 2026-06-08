fd7701457e0b8a533a611faf860ee33edab05ed0
Ren4ik7284 <bebragnom89@gmail.com>
2026-06-08 22:21:53 +0300

    Добавлена страница поддержки

--- src/app/pages/support/support.ts
+++ src/app/pages/support/support.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-support',
+  selector: "app-support",
   standalone: true,
-  templateUrl: './support.html',
-  styleUrls: ['./support.css']
+  imports: [CommonModule, RouterModule],
+  templateUrl: "./support.html",
+  styleUrls: ["./support.css"]
 })
-export class SupportComponent {}
+export class SupportComponent {
+  activeFaq: number | null = null;
+
+  toggleFaq(index: number) {
+    this.activeFaq = this.activeFaq === index ? null : index;
+  }
+}
